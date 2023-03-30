f4e08d598e85227f57a13a51094176b5269785e8
sniper19p <67769442+sniper19p@users.noreply.github.com>
2023-03-30 08:59:29 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,63 +1,81 @@
 const {
-  Client,
-  GatewayIntentBits,
-  Partials,
-  Collection,
-  EmbedBuilder,
-} = require("discord.js");
-const util = require('minecraft-server-util');
-
-const client = new Client({
-  intents: [Object.keys(GatewayIntentBits),  [GatewayIntentBits.Guilds]],
-  partials: [Object.keys(Partials)],
-});
-
-
-
-const SERVER_IP = 'DutchDeveloper.Playit.gg';
-
-client.on('ready', () => {
+    Client,
+    GatewayIntentBits,
+    Partials,
+    Collection,
+    EmbedBuilder,
+  } = require("discord.js");
+  const util = require('minecraft-server-util');
+  
+  const client = new Client({
+    intents: [Object.keys(GatewayIntentBits),  [GatewayIntentBits.Guilds]],
+    partials: [Object.keys(Partials)],
+  });
+  
+  const SERVER_IP = 'DutchDeveloper.Playit.gg';
+  const UPDATE_INTERVAL = 60000;
+  
+  let lastUpdateTimestamp = null;
+  
+  client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
-
-    setInterval(updateEmbed, 30000);
-});
-
-async function updateEmbed() {
+  
+    setInterval(updateEmbed, UPDATE_INTERVAL);
+  });
+  
+  async function updateEmbed() {
     try {
-        const response = await util.status(SERVER_IP);
-        const embed = new EmbedBuilder()
-            .setTitle('Minecraft Server Ping and Status')
-            .setDescription(`Latency to ${SERVER_IP}: ${response.roundTripLatency}ms`)
-            .addFields(
-                { name: 'Players Online', value: `${response.players.online}/${response.players.max}`, inline: true },
-                { name: 'MOTD', value: `${response.motd.clean}`, inline: true },
-                { name: 'Version', value: `${response.version.name}`, inline: true },
-            )
-            .setColor('Green');
-
-        const channel = client.channels.cache.get('1069254533416484876');
-        let message = await channel.messages.fetch({ limit: 1 }).then(messages => messages.first());
-        if (message) {
-
-            await message.delete();
-        }
+      const response = await util.status(SERVER_IP);
+      const nowTimestamp = Math.floor(Date.now() / 1000);
+  
+      const lastUpdateStr = lastUpdateTimestamp ? `<t:${lastUpdateTimestamp}:R>` : 'never';
+  
+      const embed = new EmbedBuilder()
+        .setTitle('Minecraft Server Ping and Status')
+        .setDescription(`Latency to ${SERVER_IP}: ${response.roundTripLatency}ms`)
+        .addFields(
+          { name: 'Players Online', value: `${response.players.online}/${response.players.max}`, inline: true },
+          { name: 'MOTD', value: `${response.motd.clean}`, inline: true },
+          { name: 'Version', value: `${response.version.name}`, inline: true },
+          { name: 'Last Update', value: `${lastUpdateStr}`, inline: true },
+        )
+        .setColor('Green');
+  
+      const channel = client.channels.cache.get('1090993817164980265');
+      let message = await channel.messages.fetch({ limit: 1 }).then(messages => messages.first());
+      if (message && message.author.id === client.user.id) {
+        await message.edit({ embeds: [embed] });
+      } else {
         await channel.send({ embeds: [embed] });
+      }
+  
+      lastUpdateTimestamp = nowTimestamp;
     } catch (error) {
-        const embed = new EmbedBuilder()
-            .setTitle('Error')
-            .setDescription(`Failed to ping ${SERVER_IP}`)
-            .setColor('Red');
+      const nowTimestamp = Math.floor(Date.now() / 1000);
+  
+      const lastUpdateStr = lastUpdateTimestamp ? `<t:${lastUpdateTimestamp}:R>` : 'never';
 
-        const channel = client.channels.cache.get('1069254533416484876');
-        let message = await channel.messages.fetch({ limit: 1 }).then(messages => messages.first());
-        if (message) {
-
-            await message.delete();
-        }
-
+  
+      const embed = new EmbedBuilder()
+        .setTitle('Error')
+        .setDescription(`Failed to ping ${SERVER_IP}`)
+        .addFields(
+          { name: 'Last Update', value: `${lastUpdateStr}`, inline: true },
+        )
+        .setColor('Red');
+  
+      const channel = client.channels.cache.get('1090993817164980265');
+      let message = await channel.messages.fetch({ limit: 1 }).then(messages => messages.first());
+      if (message && message.author.id === client.user.id) {
+        await message.edit({ embeds: [embed] });
+      } else {
         await channel.send({ embeds: [embed] });
+      }
+  
+      lastUpdateTimestamp = nowTimestamp;
     }
-}
+  }
 
+  
 
 client.login('MTAwMDA0NzgzNTAxMTYyOTExNg.GhyFrH.yyf6L9RQz_upmuvpAuVRtpCmBmAEQUj5K0pUP8');
